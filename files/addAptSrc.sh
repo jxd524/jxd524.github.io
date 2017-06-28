60e696fd8f527693dafcd68fe75fe213a344a13f
Terry <jxd524@163.com>
2017-06-28 16:23:18 +0800

    修改命令

--- files/addAptSrc.sh
+++ files/addAptSrc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" > /etc/apt/sources.list
-sudo echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" >> /etc/apt/sources.list
+sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" | sudo tee /etc/apt/sources.list
+sudo echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && apt-get upgrade -y
