86fdb53d30ee48b6c9c29b684bd1e8b125ed5b14
Terry <jxd524@163.com>
2017-06-28 16:43:43 +0800

    no message

--- files/addAptSrc.sh
+++ files/addAptSrc.sh
@@ -2,5 +2,8 @@
 
 sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" | sudo tee /etc/apt/sources.list
 sudo echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" | sudo tee -a /etc/apt/sources.list
+echo "begin apt-get update"
 sudo apt-get update
-apt-get upgrade -y
+
+echo "begin apt-get upgrade"
+sudo apt-get upgrade -y
