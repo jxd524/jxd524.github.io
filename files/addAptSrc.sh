a77b6eb08d35b187309fdb2f07e38f28ae8b6f9b
Terry <jxd524@163.com>
2017-06-29 09:26:05 +0800

    no message

--- files/addAptSrc.sh
+++ files/addAptSrc.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#   树莓派 apt-get 设置为清华大学的镜像源
+#
+#   作者: Terry
+#   日期: 2017-06-28 16:55
+#   Blog: http://icc.one/
+#   Email: jxd524@163.com
+#
+
 echo "set apt-get source"
 echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" | sudo tee /etc/apt/sources.list
 echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ jessie main non-free contrib" | sudo tee -a /etc/apt/sources.list
