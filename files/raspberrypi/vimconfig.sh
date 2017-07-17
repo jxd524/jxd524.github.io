67d3f7ae558502685185f2942e6328072bed1ca2
Terry <jxd524@163.com>
2017-07-17 15:26:54 +0800

    no message

--- files/raspberrypi/vimconfig.sh
+++ files/raspberrypi/vimconfig.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 
-#curl "http://icc.one/files/raspberrypi/vimrc" > ~/.vim
+mkdir ~/.vim
+mkdir ~/.vim/colors
+curl "http://icc.one/files/raspberrypi/vim.rc" > ~/.vim/vimrc
+curl "http://icc.one/files/raspberrypi/vimrc.keybindings" > ~/.vim/vimrc.keybindings
+curl "http://icc.one/files/raspberrypi/solarized.vim" > ~/.vim/colors/solarized.vim
+
